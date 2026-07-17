b8096695c6036da656885f849126f49c2468e8ab
jaswanthyeddula31@gmail.com <jaswanthyeddula31@gmail.com>
2026-07-17 22:48:47 +0530

    Fix NoneType swtrip error in config

--- app/config.py
+++ app/config.py
@@ -23,6 +23,6 @@ class Config:
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME').strip()
-    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD').strip()
+    sender_email = os.getenv('sender_email', '').strip()
+    sender_password = os.getenv('sender_password', '').strip()
 
